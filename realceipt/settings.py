6ea46000355bc2e59be802e0f86df79a79f0687f
Francis van der Giessen <francis.vandergiessen@jdecoffee.com>
2024-04-15 16:06:58 +0200

    created models Receipt, ReceiptItem, Employee and connected to MySQL database

--- realceipt/settings.py
+++ realceipt/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'realceipt.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'receipts',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'Paardenkop16!'
     }
 }
 
