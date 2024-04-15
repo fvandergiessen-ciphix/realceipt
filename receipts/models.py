de86eb49549a0178cf010ca6a97c1a0793dc3f1b
Francis van der Giessen <francis.vandergiessen@jdecoffee.com>
2024-04-15 15:55:41 +0200

    created homepage

--- receipts/models.py
+++ receipts/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 
+class Receipt(models.Model):
+    uploaded_at = models.DateTimeField(auto_now_add=True)
 
+class ReceiptItem(models.Model):
+    receipt = models.ForeignKey(Receipt, on_delete=models.CASCADE)
+    product_name = models.CharField(max_length=255)
+    product_price = models.DecimalField(max_digits=6, decimal_places=2)
 
+class Employee(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
 
